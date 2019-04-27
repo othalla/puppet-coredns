894ce67d3d22edd7349ba5bcc3b4bfb56c4a563b
othalla <othalla.lf@gmail.com>
2019-04-27 15:02:34 +0200

    Fix fact var to find architecture in init

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class coredns(
   String $version = '1.5.0',
 ) {
 
-  case $arch {
+  case $::architecture {
     'x86_64', 'amd64':            { $real_arch = 'amd64' }
     'aarch64':                    { $real_arch = 'arm64' }
     'armv7l', 'armv6l', 'armv7l': { $real_arch = 'armv' }
