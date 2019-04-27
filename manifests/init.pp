f6fc80e9be5b56d9cadcb62f6bdc971db5360507
othalla <othalla.lf@gmail.com>
2019-04-27 14:31:33 +0200

    Add architecture for url download in init

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,5 +2,14 @@ class coredns(
   Stdlib::HTTPUrl $download_url_base = 'https://github.com/coredns/coredns/releases/download/',
   String $version = '1.5.0',
 ) {
+
+  case $arch {
+    'x86_64', 'amd64':            { $real_arch = 'amd64' }
+    'aarch64':                    { $real_arch = 'arm64' }
+    'armv7l', 'armv6l', 'armv7l': { $real_arch = 'armv' }
+    default:           {
+      fail("Unsupported kernel architecture: ${arch}")
+    }
+  }
   include coredns::install
 }
