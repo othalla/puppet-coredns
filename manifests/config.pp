1329421cfe1adf11aeba0f3d59b53be2176a70e1
othalla <othalla.lf@gmail.com>
2019-04-29 13:36:45 +0200

    Use concat instead of file in config to allow plugins with defines

--- manifests/config.pp
+++ manifests/config.pp
@@ -9,8 +9,12 @@ class coredns::config {
   file { $coredns::config_dir:
     ensure => directory,
   }
-  -> file { "${coredns::config_dir}/Corefile":
-    ensure => present,
-    content => template('coredns/Corefile.erb')
+  -> concat { "${coredns::config_dir}/Corefile":
+    ensure  => present,
+  }
+  -> concat::fragment { "Corefile":
+    target  => "${coredns::config_dir}/Corefile",
+    content => template('coredns/Corefile.erb'),
+    order   => '01'
   }
 }
