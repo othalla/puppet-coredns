ecd0101e3665f3f93e20178ebd9b3bab8d735eaf
othalla <othalla.lf@gmail.com>
2019-07-17 00:33:49 +0200

    lint init

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,7 +10,7 @@ class coredns(
     'x86_64', 'amd64':            { $real_arch = 'amd64' }
     'aarch64':                    { $real_arch = 'arm64' }
     'armv7l', 'armv6l', 'armv7l': { $real_arch = 'arm' }
-    default:           {
+    default:                      {
       fail("Unsupported kernel architecture: ${::architecture}")
     }
   }
